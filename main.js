b18bab79b9f7a70a52e3f43573113c17b70e4824
AlphaDevil2018 <alphadevil2018@gmail.com>
2026-01-31 17:06:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function handleCommand(input) {
 
     switch (command) {
         case 'help':
-            document.getElementById('dialog').textContent = ('Available commands: help, hello, quit, status, version');
+            document.getElementById('dialog').textContent = ('Available commands: help, hello, quit, status, version, secret');
             break;
         case 'hello':
             document.getElementById('dialog').textContent = ('Hello, User! How can I assist you today?');
@@ -34,20 +34,38 @@ function handleCommand(input) {
         case 'version':
             document.getElementById('dialog').textContent = ('Version 1.0.0');
             break;
+        case 'secret':
+            document.getElementById('dialog').textContent = ('There are 10 hidden responses hidden in the code editor. Can you find them all?');
+            break;
         case 'upupdowndownleftrightabstart':
             document.getElementById('dialog').textContent = ("You think you're smart, don't you?");
             break;
+        case 'aaaaaaa':
+            document.getElementById('dialog').textContent = ("Screaming won't help you, you know");
+            break;
         case '67':
             document.getElementById('dialog').textContent = ('...');
+            break;
+        case 'hawaii':
+            document.getElementById('dialog').textContent = ("Hawaii is a very real place, dumbass. Shouldn't you be working?");
+            break;
+        case 'pneumonoultramicroscopicsilicovolcanoconiosis':
+            document.getElementById('dialog').textContent = ('How... the hell... did you SPELL that?! CORRECTLY?!');
+            break;
+        case 'nantendo':
+            document.getElementById('dialog').textContent = ("Genesis does what Nintendon't");
+            break;
+        case 'podcast':
+            document.getElementById('dialog').textContent = ("Stop n' crash and screech n' halt");
+            break;
+        case 'fuck':
+            document.getElementById('dialog').textContent = ("It costs NOTHING to be nice.");
             break;
         case 'bitcoin':
             document.getElementById('dialog').textContent = ('Why the hell did you forget that password?! We could be billionares right now!');
             break;
         case 'developers':
             document.getElementById('dialog').textContent = ("Okay! So what is the $64,00 quEUstion? Holy cow I've just got to tough it up now, here we go. What's the $64,000 question for the field? What is the most asked question that I've recieved this week? What the HELL are we supposed to do about .NET Steve? It's a very good question, and it's got a veeeEEeeery very clear answer. Developers. The key to .NET, the key to industry transformation, the keEEeeey to success is: developers. Developers! Developers! Developers! Developers! Developers! Developers! Developers! Developers! Developers! DevAlopers! DevAlopers! DevAlopers! DevAlopers! ...YES!");
-            break;
-        case 'aaaaaaa':
-            document.getElementById('dialog').textContent = ("Screaming won't help you, you know");
             break;
         case 'begin':
             document.getElementById('dialog').textContent = ('Are you ready to begin?');
